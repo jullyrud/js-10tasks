39cb366e0d6d3ba4952262ab8596076b060f99bf
jullyrud <94559083+jullyrud@users.noreply.github.com>
2022-11-13 15:26:05 +0100

    Update task-01.js

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,30 @@
-const allCategories = document.querySelectorAll('.item')
-console.log(`Number of categories: ${allCategories.length}`);
+// const allCategories = document.querySelectorAll('.item')
+// console.log(`Number of categories: ${allCategories.length}`);
 
 
-allCategories.forEach(element => {
+// allCategories.forEach(element => {
 
 
-    const nameOfCatogories = element.querySelectorAll('h2')
-    console.log(nameOfCatogories[0].textContent);
+//     const nameOfCatogories = element.querySelectorAll('h2')
+//     console.log(nameOfCatogories[0].textContent);
 
-    const elementsInCaregory = element.querySelectorAll('ul li') 
-    console.log(`Elements: ${elementsInCaregory.length}`);
+//     const elementsInCaregory = element.querySelectorAll('ul li') 
+//     console.log(`Elements: ${elementsInCaregory.length}`);
    
     
-});
+// });
+ 
+
+
+const findElements = document.querySelector("#categories");
+const findItems = findElements.children;
+const countCategories = () =>
+  console.log(`Number of categories: ${findElements.childElementCount}`);
+
+countCategories();
+
+const itemsCount = [...findItems].forEach((element) =>  {
+    console.log(`Category: ${element.firstElementChild.textContent}`)
+    console.log(`Elements: ${element.lastElementChild.childElementCount}`)
+
+})
